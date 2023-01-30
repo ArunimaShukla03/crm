d59160539820502b40c5316805ff006813a8db91
ArunimaShukla03 <arunimashukla2003@gmail.com>
2023-01-30 15:22:36 +0530

    models and admin

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    # We want to now see the name of the customers instead of just "Customer object(1)" in the admin panel. Thus we do the following for the same.
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
+    )
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null = True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True) 
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+class Order(models.Model):
+    # We want the dropdown menu for "status"
+
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered'),
+    )
+    #customer = 
+    #product = 
+    
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
