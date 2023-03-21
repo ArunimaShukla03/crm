9aec02c5690dd570929fc81fce880c24175aa5a2
ArunimaShukla03 <arunimashukla2003@gmail.com>
2023-03-22 01:23:54 +0530

    decorators

---
+++ crm1/accounts/decorators.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+# The first "decorator" we are gonna write is that something that stops the authenticated user from viewing the login or register page.
+
+# A decorator is a function that takes another function as an parameter.
+
+def authenticated_user(view_func):
+    def wrapper_func(request, *args, **kwargs):
+
+        return view_func(request, *args, **kwargs)
+    
+    return wrapper_func
