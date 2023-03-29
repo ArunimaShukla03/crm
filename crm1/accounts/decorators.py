4ec9a6c3160a4f19da16d259cdc42e7daa7d2207
ArunimaShukla03 <arunimashukla2003@gmail.com>
2023-03-29 23:55:50 +0530

    decorators

--- crm1/accounts/decorators.py
+++ crm1/accounts/decorators.py
@@ -8,6 +8,12 @@ from django.shortcuts import redirect
 def authenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
 
+        if request.user.is_authenticated:
+            return redirect("home")
+        
+        else:
+            return view_func(request, *args, **kwargs)
+
         return view_func(request, *args, **kwargs)
     
     return wrapper_func
