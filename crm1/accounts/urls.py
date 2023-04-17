34bc8e8211649ad89cc016e65d5cece6f30a61e8
ArunimaShukla03 <arunimashukla2003@gmail.com>
2023-04-17 22:30:53 +0530

    password reset email

--- crm1/accounts/urls.py
+++ crm1/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -22,5 +23,21 @@ urlpatterns = [
 
     path("update_order/<str:pk_test>/", views.updateOrder, name="update_order"),
 
-    path("delete_order/<str:primary_key>", views.deleteOrder, name="delete_order")
+    path("delete_order/<str:primary_key>", views.deleteOrder, name="delete_order"),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
+
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+
+    # "reset/" is the URL endpoint that triggers the password reset process.
+
+    # "<uidb64>" is the unique identifier that is used to identify the user requesting the password reset.
+
+    # "<token>" is the one-time token that is generated and sent to the user's email for security purposes.
+
+    # When you click on the link containing the token, "auth_views.PasswordResetConfirmView" view verifies the validity of the token and allows the user to reset their password.
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -144,3 +144,12 @@ STATICFILES_DIR = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR/'static/images')
+
+# SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'arunimashukla2003@gmail.com'
+EMAIL_HOST_PASSWORD = 'xgerzzsstsawbdgx'
